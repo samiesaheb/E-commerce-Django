f957fa27939357f55364e16b05e3dc6f6724da74
Samie Saheb <shs.priv@gmail.com>
2025-06-18 22:19:11 +0700

    Add custom auth forms

--- skyhigh/core/forms.py
+++ skyhigh/core/forms.py
@@ -1,7 +1,11 @@
 # core/forms.py
 
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    PasswordChangeForm,
+)
 from django.contrib.auth.models import User
 
 
@@ -25,6 +29,28 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update(
+                {"class": "w-full border border-gray-300 rounded p-2"}
+            )
+
+
+class CustomLoginForm(AuthenticationForm):
+    """Login form with Tailwind CSS styling."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update(
+                {"class": "w-full border border-gray-300 rounded p-2"}
+            )
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    """Password change form with Tailwind CSS styling."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
