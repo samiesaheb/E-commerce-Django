25d8bed6ace1d02b7ad5f6e3857f2d1afa1e6aff
Samie Saheb <shs.priv@gmail.com>
2025-06-19 22:50:00 +0700

    Add files via upload

--- skyhigh/skyhigh/settings.py
+++ skyhigh/skyhigh/settings.py
@@ -105,6 +105,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "core.context_processors.cart_item_count",
+                'core.context_processors.cart_items',
             ],
         },
     },
